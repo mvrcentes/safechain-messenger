602318a5c54265f2b557a6893dc46eb5f162139b
Fabian Juarez <jua21440@uvg.edu.gt>
2025-09-25 22:02:57 -0600

    fix some problems
    found some problems with owasp and fix with this code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import express from "express"
+import helmet from "helmet"
 
 import authRoutes from "./routes/auth.routes.js"
 import groupRoutes from "./routes/group.routes.js"
@@ -15,10 +16,42 @@ const corsOptions = {
   origin: `http://localhost:${process.env.FRONTEND_LOCAL_PORT || 5173}`,
   credentials: true,
 }
+app.use(cors(corsOptions))
+
+// Remover header por defecto
+app.disable("x-powered-by")
+app.set("port", process.env.PORT || 5000)
+
+// Helmet con configuraciones específicas
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: [],
+      },
+    },
+    frameguard: { action: "deny" }, // Anti-clickjacking
+    xssFilter: true,                // Protege contra XSS reflejado
+    noSniff: true,                  // X-Content-Type-Options: nosniff
+    hsts: { maxAge: 31536000, includeSubDomains: true, preload: true }, // HSTS
+  })
+)
+
+// Middleware para cache-control en info sensible
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-store")
+  res.setHeader("Pragma", "no-cache")
+  res.setHeader("Expires", "0")
+  res.removeHeader("Server") // elimina header "Server"
+  next()
+})
+// ------------------------------------------ //
 
 app.set("port", process.env.PORT || 5000)
 
-app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
 app.use("/api/auth", authRoutes)
